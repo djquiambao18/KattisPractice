babe71096b7b5bdb701f44efd62922e97af90bce
Darwish <djquiambao.18@gmail.com>
2022-04-12 00:52:56 -0700

    runtime errors

--- Aaaah!/Aaaah.java
+++ Aaaah!/Aaaah.java
@@ -2,23 +2,46 @@ import java.util.Scanner;
 import java.util.HashMap;
 class Aaah{
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         String[] str = new String[2];
-        str[0] = in.nextLine(); //what the guy can do that day
-        str[1] = in.nextLine(); //what the dr. needs
+        str[0] = input.nextLine(); //what the guy can do that day
+        str[1] = input.nextLine(); //what the dr. needs
+        input.close();
         if(str[0].length() >= str[1].length() && str[0].charAt(str[0].length()-1) == 'h' && str[1].charAt(str[0].length()-1) == 'h' 
-            && str[0].charAt(0) == 'a' && str[1].charAt(0) == 'a'){
+            && str[0].charAt(0) == 'a' && str[1].charAt(0) == 'a' && str[0].length() > 0 && str[1].length() > 0){
             char [] ch1 = str[0].toCharArray();
             char [] ch2 = str[1].toCharArray();
             int i = 0;
-            HashMap<Character, Charac
-            while(ch1[i] == 'a' || ch1[i] == 'h' && i < ch1.length){
-                
+            HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+            while(i < ch1.length && ch1[i] == 'a' || ch1[ch1.length-1] == 'h'){
+                if(map.containsKey(ch1[i])){
+                    map.put(ch1[i], map.get(ch1[i])+1);
+                }
+                else{
+                    map.put(ch1[i], 1);
+                }
+                i++;
             }
-            
+            int count = 0;
+            i = 0;
+            while(i < ch2.length && ch2[i] == 'a' && ch2[ch2.length-1] == 'h'){
+                count++;
+                i++;
+                if(i < ch2.length && ch2[i] != 'a' && ch2[ch2.length-1] != 'h'){
+                    System.out.println("no");
+                    return;
+                }
+            }
+            if(count <= map.get('a')){
+                System.out.println("go");
+            }
+            else{
+                System.out.println("no");
+            }
         }
         else{
             System.out.println("no");
         }
+
     }
 }
